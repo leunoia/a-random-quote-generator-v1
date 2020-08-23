c2691633c2710d5250e943f456eb5e6c87d2ca0b
leunoia <68248433+leunoia@users.noreply.github.com>
2020-08-22 20:25:45 -0400

    Array Created

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {Quote:"The greatest glory in living lies not in never falling, but in rising every time we fall." , Source: 'Nelson Mandela', Citation: '', year:''},
+  {Quote:"The way to get started is to quit talking and begin doing." , Source: 'Walt Disney', Citation: '', year:'' },
+  {Quote:"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking." ,Source:'Steve Jobs', Citation: '', year:'' },
+  {Quote:"If life were predictable it would cease to be life, and be without flavor."  , Source:'Eleanor Roosevelt', Citation: '', year:'' },
+  {Quote:"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough." , Source:'Oprah Winfrey', Citation: '', year:'' },
+  {Quote:"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."  , Source:'James Cameron', Citation: '', year:'' },
+  {Quote:"When you reach the end of your rope, tie a knot in it and hang on."  , Source:'Franklin D. Roosevelt', Citation: '', year:'' },
+  {Quote:"Always remember that you are absolutely unique. Just like everyone else."  , Source:'Margaret Mead', Citation: '', year:'' },
+  {Quote:"Don't judge each day by the harvest you reap but by the seeds that you plant." , Source:'Robert Louis Stevenson', Citation: '', year:'' },
+  {Quote:"The future belongs to those who believe in the beauty of their dreams."  , Source:'Eleanor Roosevelt', Citation: '', year:'' }
+]
+
 /***
  * `getRandomQuote` function
 ***/
